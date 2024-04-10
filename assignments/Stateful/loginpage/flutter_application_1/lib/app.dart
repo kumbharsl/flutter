eec55c4772a6ef0fa76bc1fc8a5e1329c6aaa191
Sagar Kumbhar <sagarkumbhar2018@gmail.com>
2024-04-10 23:10:26 +0530

    done

---
+++ assignments/Stateful/loginpage/flutter_application_1/lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  @override
+  State createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        // body:
+
+        );
+  }
+}
