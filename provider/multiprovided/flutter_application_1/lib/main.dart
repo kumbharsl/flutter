8e4abd014574eb722afdfe2db0b9c3db7fa9ae76
Sagar Kumbhar <sagarkumbhar2018@gmail.com>
2024-04-24 14:11:47 +0530

    multiprovider donne

--- provider/multiprovided/flutter_application_1/lib/main.dart
+++ provider/multiprovided/flutter_application_1/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    log("IN BUILD MY APP");
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) {
+          return Employee(empName: "Kanha", empId: 10);
+        }),
+        ChangeNotifierProvider(create: (context) {
+          return Project(projectName: "HealthCare", devType: "Backend Dev");
+        }),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
       ),
     );
   }
---
+++ provider/multiprovided/flutter_application_1/lib/real.dart
@@ -0,0 +1,113 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    log("IN BUILD MY APP");
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) {
+          return Employee(empName: "Kanha", empId: 10);
+        }),
+        ChangeNotifierProvider(create: (context) {
+          return Project(projectName: "HealthCare", devType: "Backend Dev");
+        }),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State createState() => _HomePage();
+}
+
+class _HomePage extends State {
+  @override
+  Widget build(BuildContext context) {
+    log("IN BUILD FRAMEWORK");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("MultiProvider State Mgmt"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              Provider.of<Employee>(context).empName,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              "${Provider.of<Employee>(context).empId}",
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              Provider.of<Project>(context).projectName,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              Provider.of<Project>(context).devType,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue.shade400),
+              ),
+              onPressed: () {
+                Provider.of<Project>(context, listen: false)
+                    .changeProject("EdTech", "Mibile Dev");
+              },
+              child: const Text(
+                "Change Project",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Employee {
+  String empName;
+  int empId;
+  Employee({required this.empName, required this.empId});
+}
+
+class Project with ChangeNotifier {
+  String projectName;
+  String devType;
+  Project({required this.projectName, required this.devType});
+
+  void changeProject(String projectName, String devType) {
+    this.projectName = projectName;
+    this.devType = devType;
+    notifyListeners();
+  }
+}
